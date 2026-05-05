7e3cdc1c46bca2fdd0a0fcb07d8f1d7456f085e3
ruslan <runovikov2@gmail.com>
2026-05-06 00:50:04 +0300

    добавить LLMRepository и AnswerRepository

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,1 +1,3 @@
+from .answer import AnswerRepository
+from .llm import LLMRepository
 from .task import TaskRepository
--- src/repositories/llm.py
+++ src/repositories/llm.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models import TaskORM
+from models import LLMOrm
 
 from .base import BaseRepository
 
 
-class TaskRepository(BaseRepository[TaskORM]):
+class LLMRepository(BaseRepository[LLMOrm]):
     def __init__(self, session: AsyncSession):
-        super().__init__(TaskORM, session)
+        super().__init__(LLMOrm, session)
